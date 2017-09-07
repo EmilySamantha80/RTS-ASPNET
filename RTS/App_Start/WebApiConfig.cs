92714408431a022d50bb3e954ea6c2327c2d1e8b
emilysamantha80 <emilysamantha80@gmail.com>
2017-09-07 08:24:04 -0700

    Initial solution commit

---
+++ RTS/App_Start/WebApiConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.WebHost;
+using System.Web.Routing;
+using System.Web.SessionState;
+
+namespace RTS
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            var route = RouteTable.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = System.Web.Http.RouteParameter.Optional }
+            );
+            route.RouteHandler = new MyHttpControllerRouteHandler();
+        }
+
+        public class MyHttpControllerHandler : HttpControllerHandler, IRequiresSessionState
+        {
+            public MyHttpControllerHandler(RouteData routeData) : base(routeData)
+            { }
+        }
+
+        public class MyHttpControllerRouteHandler : HttpControllerRouteHandler
+        {
+            protected override IHttpHandler GetHttpHandler(RequestContext requestContext)
+            {
+                return new MyHttpControllerHandler(requestContext.RouteData);
+            }
+        }
+    }
+}
